8d8d7adb5994ecad0aadd0a24f465416e36f5e3d
LukasBartheldi <kutalukas@seznam.cz>
2023-01-27 00:33:43 +0100

    Basic computer functionality

--- functions.js
+++ functions.js
@@ -1,19 +1,112 @@
-function clicked (element, elementDel1, elementDel2) {
+function clicked(element, elementDel1, elementDel2) {
 
-       new Audio(`audio/click.wav`).play()
+  new Audio(`audio/click.wav`).play()
 
-       setTimeout(() => {
-        elementDel1.remove()
-        elementDel2.remove()
-      }, 2000)
- 
-      setTimeout(() => {
-        element.style.width = "50%"
-        element.style.height = "60%"
-        element.style.transition = "height 1s, width 1s"
-      }, 2500)
+  setTimeout(() => {
+    elementDel1.remove()
+    elementDel2.remove()
+  }, 2000)
 
-      setTimeout(() => {
-     element.style.border = "0"
-      }, 4000)
+  setTimeout(() => {
+    element.style.width = "50%"
+    element.style.height = "60%"
+    element.style.transition = "height 1s, width 1s"
+  }, 2500)
+
+  setTimeout(() => {
+    element.style.border = "0"
+  }, 4000)
+}
+
+
+
+function shuffleOptions() {
+
+  if (endShuffle < 10) {
+
+    //paper
+    setTimeout(() => {
+      computer[1].classList.remove("rock")
+      computer[1].classList.add("rock-none")
+      computer[2].classList.remove("scissors")
+      computer[2].classList.add("scissors-none")
+      computer[0].classList.remove("paper-none")
+      computer[0].classList.add("paper")
+      console.log(computer[0])
+    }, 150)
+
+
+    // rock
+    setTimeout(() => {
+      computer[0].classList.remove("paper")
+      computer[0].classList.add("paper-none")
+      computer[2].classList.remove("scissors")
+      computer[2].classList.add("scissors-none")
+      computer[1].classList.remove("rock-none")
+      computer[1].classList.add("rock")
+      console.log(computer[1])
+    }, 300)
+
+    //scissors
+    setTimeout(() => {
+      computer[0].classList.remove("paper")
+      computer[0].classList.add("paper-none")
+      computer[1].classList.remove("rock")
+      computer[1].classList.add("rock-none")
+      computer[2].classList.remove("scissors-none")
+      computer[2].classList.add("scissors")
+      console.log(computer[2])
+    }, 450)
+
+
+
+    endShuffle++
+    console.log(endShuffle)
+    setTimeout(shuffleOptions, 600)
+  }
+
+  computerWinOption()
+}
+
+
+
+function computerWinOption() {
+
+  console.log(getRandom0To2())
+  randomNumber = getRandom0To2()
+
+  //paper
+  if (randomNumber === 0) {
+    computer[1].classList.remove("rock")
+    computer[1].classList.add("rock-none")
+    computer[2].classList.remove("scissors")
+    computer[2].classList.add("scissors-none")
+    computer[0].classList.remove("paper-none")
+    computer[0].classList.add("paper")
+  } else
+    //rock
+    if (randomNumber === 1) {
+      computer[0].classList.remove("paper")
+      computer[0].classList.add("paper-none")
+      computer[2].classList.remove("scissors")
+      computer[2].classList.add("scissors-none")
+      computer[1].classList.remove("rock-none")
+      computer[1].classList.add("rock")
+    } else
+      //scissors
+      if (randomNumber === 2) {
+        computer[0].classList.remove("paper")
+        computer[0].classList.add("paper-none")
+        computer[1].classList.remove("rock")
+        computer[1].classList.add("rock-none")
+        computer[2].classList.remove("scissors-none")
+        computer[2].classList.add("scissors")
+      }
+}
+
+
+
+
+function getRandom0To2() {
+  return Math.floor(Math.random() * 3);
 }
--- script.js
+++ script.js
@@ -1,49 +1,71 @@
-let paper = document.querySelector(".player-side")
+let playerSide = document.querySelector(".player-side")
+let computerSide = document.querySelector(".computer-side")
 
 let player = new Array(3)
-player["paper"] = paper.children[0]
-player["rock"] = paper.children[1]
-player["scissors"] = paper.children[2]
+player["paper"] = playerSide.children[0]
+player["rock"] = playerSide.children[1]
+player["scissors"] = playerSide.children[2]
 
-let goThrow = 0
+let computer = []
+for (let i = 0; i < 3; i++) {
+    computer.push(computerSide.children[i])
+}
+
+
+let goThrow = false
+let randomNumber
+let endShuffle = 0
+
 
 
 player["paper"].addEventListener("click", function (e) {
 
-    if (goThrow == 0) {
+    if (goThrow === false) {
         player["paper"].classList.add("player-side-paper")
         player["rock"].classList.add("player-side-rock-none")
         player["scissors"].classList.add("player-side-scissors-none")
 
         clicked(player["paper"], player["rock"], player["scissors"])
+
+        setTimeout(() => {
+            shuffleOptions()
+        }, 4000)
     }
-    goThrow = 1
+    goThrow = true
 })
 
 
 
 player["rock"].addEventListener("click", function (e) {
 
-    if (goThrow == 0) {
+    if (goThrow === false) {
         player["paper"].classList.add("player-side-paper-none")
         player["rock"].classList.add("player-side-rock")
         player["scissors"].classList.add("player-side-scissors-none")
 
         clicked(player["rock"], player["paper"], player["scissors"])
+
+        setTimeout(() => {
+            shuffleOptions()
+        }, 4000)
     }
-    goThrow = 1
+    goThrow = true
 })
 
 
 
 player["scissors"].addEventListener("click", function (e) {
 
-    if (goThrow == 0) {
+    if (goThrow === false) {
         player["paper"].classList.add("player-side-paper-none")
         player["rock"].classList.add("player-side-rock-none")
         player["scissors"].classList.add("player-side-scissors")
 
         clicked(player["scissors"], player["rock"], player["paper"])
+
+        setTimeout(() => {
+            shuffleOptions()
+        }, 4000)
     }
-    goThrow = 1
+    goThrow = true
 })
